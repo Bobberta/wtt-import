bebe11867e9d4c374bf8c5298fc1f6773b186a7f
Bobberta <anais.limpalaer@gmail.com>
2020-05-16 18:15:57 +0200

    add ability to export to JSON

--- client/src/components/ExportStep.tsx
+++ client/src/components/ExportStep.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Result, Button } from "antd";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/reducers";
 
 const ExportStep = () => {
-  const handleJSONDownload = () => {};
+  const tracks = useSelector((state: RootState) => state.tracks);
 
-  const handleCSVDownload = () => {};
+  const JSONtracks = `text/json;charset=utf-8,${encodeURIComponent(
+    JSON.stringify(tracks)
+  )}`;
 
   return (
     <Result
@@ -12,11 +16,13 @@ const ExportStep = () => {
       title="L'import est terminé !"
       subTitle="Tu peux maintenant télécharger le fichier final au format de ton choix."
       extra={[
-        <Button key="1" type="primary" onClick={handleJSONDownload}>
+        <Button
+          key="1"
+          type="primary"
+          href={`data:'${JSONtracks}'`}
+          download="wtt-playlist.json"
+        >
           Télécharger au format JSON
-        </Button>,
-        <Button key="2" onClick={handleCSVDownload}>
-          Télécharger au format CSV
         </Button>,
       ]}
     />
